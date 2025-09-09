70340d709b6d1b91786e5fe944c60e6d3d7e8875
Lordink <infernalstudios@gmail.com>
2025-09-09 20:37:46 +0700

    Echoing back

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <vector>
 
 using std::cerr;
 using std::cout;
 using std::endl;
 using std::getline;
+using std::string;
 
 int main() {
     // Flush after every std::cout / std:cerr
@@ -14,11 +17,25 @@ int main() {
         cerr << std::unitbuf;
         cout << "$ ";
 
-        std::string input;
+        string input;
         getline(std::cin, input);
+
+        // Break into words:
+        string word;
+        std::istringstream iss(input, std::istringstream::in);
+        std::vector<string> words;
+
+        while (iss >> word) {
+            words.push_back(word);
+        }
 
         if (input == "exit 0") {
             break;
+        } else if (words.size() > 1 && words[0] == "echo") {
+            for (size_t i = 1; i < words.size(); i++) {
+                cout << words[i] << ' ';
+            }
+            cout << endl;
         } else {
             cout << input << ": command not found" << endl;
         }
